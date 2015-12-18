958ab2d78c1218c26acc6f2822df3b90f9b04a2b
gulci <sebastian.gulczynski.poznan@gmail.com>
2015-12-18 13:43:54 +0100

    2015-12-18 GradeBook app, classes chapter

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,11 +8,61 @@ namespace Grades
 {
     class GradeBook
     {
+        // ctor + tab * 2
+        // upper case for public members
+        // konstruktor to jedyna metoda, w której nie podajemy zwracanego typu
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
+        public List<float> getGrades()
+        {
+            return grades;
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            // użycie funkcji wbudowanych w listę
+            stats.setAverage(grades.Average());
+            stats.setHighestGrade(grades.Max());
+            stats.setLowestGrade(grades.Min());
+
+            /*
+            if (grades.Count() > 0)
+            { 
+                float sum = 0;
+                foreach(float grade in grades)
+                {
+                    stats.setHighestGrade(Math.Max(grade, stats.getHighestGrade()));
+                    stats.setLowestGrade(Math.Min(grade, stats.getLowestGrade()));
+                    sum += grade;
+                }
+
+                stats.setAverage(sum / grades.Count());
+            }
+            else
+            {
+                // zerujemy tylko najniższą wartość, nie będzie już potrzebna do porównań
+                // average i highestGrade są zerowane w konstruktorze
+                stats.setLowestGrade(0);
+            }
+            */
+            
+            return stats;
+        }
+
+        public static float MinimumGrade = 0;
+        public static float MaximumGrade = 100;
+
+        // domyślnie private
         List<float> grades;
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -10,6 +10,30 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            // shift + f5 - przerwanie debugowania
+
+            // klasa jest typem referencyjnym, w zmiennej przechowujemy adres w pamięci (wskaźnik)
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(95);
+            book.AddGrade(50.1f);
+
+            GradeBook book2 = book;
+            book.AddGrade(75);
+            book.AddGrade(33.3f);
+
+            Console.WriteLine("Minimum grade: " + GradeBook.MinimumGrade);
+            Console.WriteLine("Maximum grade: " + GradeBook.MaximumGrade);
+
+            List<float> currentGrades = book.getGrades();
+            Console.WriteLine("Last grade added: " + currentGrades.ElementAt(currentGrades.Count() - 1));
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            // cw + tab * 2
+            Console.WriteLine("Average: " + stats.getAverage());
+            Console.WriteLine("Highest grade: " + stats.getHighestGrade());
+            Console.WriteLine("Lowest grade: " + stats.getLowestGrade());
         }
     }
 }
